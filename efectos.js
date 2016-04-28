3de45ef70b5dc0b55feec8cc97ff8f81ca68ddd3
Escuela Digital <ceo@escueladigital.pe>
2016-04-28 11:46:12 -0500

    login terminado se agregaron unos efectos simples con javascript

---
+++ efectos.js
@@ -0,0 +1,16 @@
+// login form
+(function(){
+  var loginForm = document.querySelector('.login-form');
+  if(loginForm){
+    var input = loginForm.querySelectorAll('.input');
+    for(var i = 0; i < 2; i++){
+      input[i].addEventListener('blur', function(){
+        if(!this.value == null || !this.value == "") {
+          this.classList.add('focus');
+        } else {
+          this.classList.remove('focus');
+        }
+      })
+    }
+  }
+})();
