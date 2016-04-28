36983c060923149052cfc8a1742d328cc221f513
Escuela Digital <ceo@escueladigital.pe>
2016-04-28 18:17:29 -0500

    gulp-watch anadido se anadio el plugin gulp-watch para que gulp detecte archivos nuevos sin reiniciar las tareas

--- efectos.js
+++ efectos.js
@@ -5,7 +5,7 @@
     var input = loginForm.querySelectorAll('.input');
     for(var i = 0; i < 2; i++){
       input[i].addEventListener('blur', function(){
-        if(!this.value == null || !this.value == "") {
+        if(this.value != null || this.value != "") {
           this.classList.add('focus');
         } else {
           this.classList.remove('focus');
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const gulp         = require('gulp'),
       sass         = require('gulp-sass'),
       jade         = require('gulp-jade'),
       autoprefixer = require('gulp-autoprefixer'),
-      sync         = require('browser-sync').create();
+      broswerSync  = require('browser-sync').create(),
+      watch        = require('gulp-watch');
 
 gulp.task('sass', () =>
   gulp.src('./sass/estilos.scss')
+    .pipe(watch('sass/**/*.scss'))
     .pipe(sass({
       outputStyle: 'compressed'
     }))
@@ -14,22 +16,24 @@ gulp.task('sass', () =>
       cascade: false
     }))
     .pipe(gulp.dest('./'))
-    .pipe(sync.stream())
+    .pipe(broswerSync.stream())
 );
 
 gulp.task('jade', () =>
   gulp.src('./jade/*.jade')
+      .pipe(watch('./jade/**/*.jade'))
       .pipe(jade({
         pretty: true
       }))
       .pipe(gulp.dest('./'))
-      .on('end', sync.reload)
+      .on('end', broswerSync.reload)
 );
 
 gulp.task('default', () => {
-  sync.init({
+  broswerSync.init({
     server: './'
   });
   gulp.watch('./sass/**/*.scss', ['sass']);
   gulp.watch('./jade/**/*.jade', ['jade']);
+  gulp.watch('./efectos.js').on('change', broswerSync.reload);
 });
